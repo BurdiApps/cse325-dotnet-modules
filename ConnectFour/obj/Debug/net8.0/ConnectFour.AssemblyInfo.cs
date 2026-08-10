4f6db4fa772f1804f4f34cf751c8474a8baba750
BurdiApps <aburdick@byupathway.edu>
2026-08-09 17:07:07 -0700

    Finished connect four tutorial and added my own lowest moves high score feature

--- ConnectFour/obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
+++ ConnectFour/obj/Debug/net8.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b73bf74af9f6f40dfbec8adf9e453f0b6cddef26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f32c531c4c16aa2e287dda0facc44d93e27bf14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
